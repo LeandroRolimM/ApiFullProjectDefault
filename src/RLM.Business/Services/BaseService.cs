488c2726bc9b8d1b70217b44aeed218b0ad0e087
LEANDRO ROLIM <rolim.rlm@hotmail.com>
2022-01-14 15:49:22 -0300

    Adicionada ServiceBase, classe base de serviço da camada de negocios

---
+++ src/RLM.Business/Services/BaseService.cs
@@ -0,0 +1,49 @@
+﻿using RLM.Business.Interfaces;
+using RLM.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using RLM.Business.Notifications;
+using FluentValidation.Results;
+
+namespace RLM.Business.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotificador _notificador;
+
+        public BaseService(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+
+        protected bool ExecutarValidacao<TEntity, TValidator>(TEntity entity, TValidator validator) where TValidator : AbstractValidator<TEntity> where TEntity : Entity
+        {
+            var result = validator.Validate(entity);
+
+            if (result.IsValid) return true;
+
+            NotificarErro(result);
+
+            return false;
+        }
+
+
+        protected void NotificarErro(ValidationResult validationResult)
+        {
+            foreach (var erro in validationResult.Errors)
+            {
+                NotificarErro(erro.ErrorMessage);
+            }
+        }
+
+        protected void NotificarErro(string erro)
+        {
+            _notificador.Handle(new Notificacao(erro));
+        }
+
+    }
+}
