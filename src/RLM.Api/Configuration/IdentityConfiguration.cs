2c6bd70df7de776216ab5bd2cd8a320ad265ed97
LEANDRO ROLIM <rolim.rlm@hotmail.com>
2022-01-14 10:47:34 -0300

    Adicionado configuraaçoes do identity

---
+++ src/RLM.Api/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RLM.Api.Data;
+using RLM.Api.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RLM.Api.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, 
+                                                                  IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddErrorDescriber<IdentityMensagensEmPortugues>()
+                .AddDefaultTokenProviders();
+
+
+            return services;
+        }
+    }
+}
