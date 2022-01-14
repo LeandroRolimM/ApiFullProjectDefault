f9cde2ac4c0220e0da5cd8b7e357b87a5b10e974
LEANDRO ROLIM <rolim.rlm@hotmail.com>
2022-01-14 14:59:38 -0300

    Adicionado Repositório generico

--- src/RLM.Business/Interfaces/IRepository.cs
+++ src/RLM.Business/Interfaces/IRepository.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using RLM.Business.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace RLM.Business.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<TEntity>: IDisposable where TEntity:Entity
     {
+        Task<IEnumerable<TEntity>>Buscar(Expression<Func<TEntity,bool>> predicate);
+        Task<TEntity> ObterPorId(Guid id);
+        Task<IEnumerable<TEntity>> ObterTodos();
+        Task Adicionar(TEntity entity);
+        Task Atualizar(TEntity entity);
+        Task Remover(TEntity entity);
+        Task<int> SaveChanges();
     }
 }
---
+++ src/RLM.Data/Repositories/Repository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RLM.Business.Interfaces;
+using RLM.Business.Models;
+using RLM.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLM.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected readonly DbSet<TEntity> DbSet;
+        protected readonly SolutionDbContext Db;
+
+        public Repository(SolutionDbContext dbContext)
+        {
+            Db = dbContext;
+            DbSet = Db.Set<TEntity>();
+        }
+
+        public virtual async Task Adicionar(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Atualizar(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Remover(TEntity entity)
+        {
+            DbSet.Remove(entity);
+            await SaveChanges();
+        }
+
+        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<TEntity> ObterPorId(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> ObterTodos()
+        {
+            return await DbSet.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
+    }
+}
